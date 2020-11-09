e2a57eee472e78949e792e297cbff33e164a39ab
Doron Eli <doron050@gmail.com>
2020-11-09 21:08:15 +0200

    convert user model to UserIndex & add search by email

---
+++ Models/UserIndex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assetify.Models
+{
+    public class UserSearch
+    {
+        public string Email { get; set; }
+    }
+
+    public class UserIndex
+    {
+        public IEnumerable<User> users { get; set; }
+        public UserSearch userSearch { get; set; }
+
+    }
+}
