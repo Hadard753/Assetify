0c04ccaa7b291cf815d7cbef3dd5aab788d21e13
Doron Eli <doron050@gmail.com>
2020-11-09 10:07:03 +0200

    post to facebook on create asset

---
+++ Models/CreateAssetRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assetify.Models
+{
+    public class CreateAssetRequest : Asset
+    {
+        public bool PostToFacebook { get; set; }
+    }
+}
