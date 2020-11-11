55b1bcb211d78218d3bb4894ab9dfe03afb461d1
Doron Eli <doron050@gmail.com>
2020-11-11 11:23:00 +0200

    add first name and last name to users search

--- Models/UserIndex.cs
+++ Models/UserIndex.cs
@@ -8,6 +8,8 @@ namespace Assetify.Models
     public class UserSearch
     {
         public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 
     public class UserIndex
